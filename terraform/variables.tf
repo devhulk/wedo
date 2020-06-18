78aa52ca0a22bf6cc8f8f8063293677133aa90cb
Gerald Yerden II <devhulk@Geralds-MBP.carolina.rr.com>
2020-06-18 11:22:07 -0400

    changed required root module variables - rm failover rpl

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,8 +6,3 @@ variable "resource_group_location" {
     type = "string"
     description = "Azure region that resource group will be deployed to."
 }
-
-variable "failover_location" {
-    type = "string"
-    description = "The name of the Azure region to host replicated data."
-}
